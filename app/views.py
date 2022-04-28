e541c7cd42cc5389e1475263f30c22f47048faf2
Sammy Muchiri <dev.miano@gmail.com>
2022-04-28 05:12:07 +0300

    added the search functionality

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from re import U
+from flask import render_template,request,redirect,url_for
 from app import app
-from .request import get_movies,get_movie
+from .request import get_movies,get_movie,search_movie
+
 
 
 @app.route('/')
@@ -10,9 +12,13 @@ def index():
   upcoming_movies = get_movies('upcoming')
   now_showing_movie = get_movies('now_playing')
   print(popular_movies)
-  message = 'Hello Flask!'
   title = 'Home - Welcome to The best Movie Review Website Online'
-  return render_template('index.html', message=message, title=title, popular=popular_movies, upcoming=upcoming_movies, now_showing=now_showing_movie)
+  search_movie = request.args.get('movie_query')
+  
+  if search_movie:
+    return redirect(url_for('search', movie_name=search_movie))
+  else:
+    return render_template('index.html', title=title, popular=popular_movies, upcoming=upcoming_movies, now_showing=now_showing_movie)
 
 @app.route('/movie/<int:id>')
 def movie(id):
@@ -20,3 +26,16 @@ def movie(id):
   title = f'{movie.title}'
   
   return render_template('movie.html', title = title, movie = movie)
+
+
+@app.route('/search/<movie_name>')
+def search(movie_name):
+  '''Function to display search results for a given movie'''
+  
+  movie_name_list = movie_name.split(' ')
+  movie_name_format = '+'.join(movie_name_list)
+  searched_movies = search_movie(movie_name_format)
+  title = f'search results for {movie_name}'
+  
+  return render_template('search.html', movies=searched_movies)
+  
